dd2bc8e95dca2568e95bcafd0bb2ed91bdcdc13f
Max Korlaar <max@maxkorlaar.com>
2016-09-27 19:22:12 +0200

    Clean up code, reformat, add .gitignore.

--- src/me/mrfahrenheit/fruitninja/Game.java
+++ src/me/mrfahrenheit/fruitninja/Game.java
@@ -22,140 +22,141 @@ import java.util.List;
 import java.util.Random;
 
 
-public class Game implements Listener{
-	
-	static final double distance = 3.4;
+public class Game implements Listener {
 
-	
-	int tickTask;
-	
-	Location gameLocation;
-	
-	Player player;
-
-	
-	char	rainbowTick	= 0;
-	
-	enum RainBowColor{
-		RED,BLUE,GREEN;
-	}
-	
-	RainBowColor currentcolor = RainBowColor.RED;
-	
-	public int score = 0;
-	
-	boolean clicking = false;
-	int clickingdelay = 0;
-
-	public Game(Player player, Location location) {
-		this.player = player;
-		gameLocation = location;
-		start();
-	}
-
-	public void addPoint() {
-		score++;
-		player.setLevel(score);
-	}
-
-	public int lives = 5;
-
-	public void removeLife() {
-		lives--;
-		if (lives == 0) {
-			stop();
-		}
-	}
-
-	public int maxDelay = 80;
-	public int delay = 20;
-	
-	public void tick() {
-		if (clickingdelay++ > 5) clicking = false;
-		if (delay-- < 0) {
-			delay = new Random().nextInt(maxDelay);
-			if (maxDelay > 20) maxDelay--;
-			fruitlist.add(new Fruit(this));
-		}
-		Location location = lookingLocation();
-		if (rainbowTick++ == 255) {
-			rainbowTick = 0;
-		}
-		if (clicking) {
-			displayParticle(location, Color.getHSBColor(rainbowTick/255F, 0.5F,0.5F));
-		}
-		for (int i = 0;i<fruitlist.size();i++) if (fruitlist.get(i).tick(location)) {
-			fruitlist.remove(i);
-		}
-	}
-
-	public Location lookingLocation() {
-		double z = distance+gameLocation.getZ();
-		double playerdistance = player.getLocation().getZ()-z;
-		double y = playerdistance*Math.tan(Math.toRadians(player.getLocation().getPitch()))+1.7+player.getLocation().getY();
-		double x = playerdistance*Math.tan(Math.toRadians(player.getLocation().getYaw()))+player.getLocation().getX();
-		return new Location(player.getWorld(), x,y,z);
-	}
-	
-	public void displayParticle(Location location, Color color) {
-		((CraftPlayer) player).getHandle().playerConnection.sendPacket(
-				new PacketPlayOutWorldParticles(EnumParticle.REDSTONE, false, (float) location.getX(),
-						(float) location.getY(), (float) location.getZ(), (float) (color.getRed()+1)/255-1,
-						(float) (color.getGreen()+1)/255, (float) (color.getBlue()+1)/255, 1F, 0));
-	}
+    static final double distance = 3.4;
 
 
-	@EventHandler
-	public void onClick(PlayerInteractEvent event) {
-		if (event.getPlayer() == player) {
-			clicking = true;
-			clickingdelay = 0;
-			event.setCancelled(true);
-		}
-	}
+    int tickTask;
 
-	@EventHandler
-	public void onClickArEntity(PlayerInteractAtEntityEvent event) {
-		if (event.getPlayer() == player) {
-			clicking = true;
-			clickingdelay = 0;
-			event.setCancelled(true);
+    Location gameLocation;
 
-		}
-	}
+    Player player;
 
-	@EventHandler
-	public void onClickEntity(PlayerInteractEntityEvent event) {
-		if (event.getPlayer() == player) {
-			clicking = true;
-			clickingdelay = 0;
-			event.setCancelled(true);
 
-		}
-	}
+    char rainbowTick = 0;
 
-	
-	void start() {
-		tickTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(Fruitninja.i(), this::tick, 1, 1);
-		player.teleport(gameLocation);
-		player.setGameMode(GameMode.ADVENTURE);
-		player.setItemInHand(new ItemStack(Material.BONE));
-		register();
-	}
+    enum RainBowColor {
+        RED, BLUE, GREEN;
+    }
 
-	public List<Fruit> fruitlist = new ArrayList<>();
+    RainBowColor currentcolor = RainBowColor.RED;
 
-	public void register() {
-		Bukkit.getPluginManager().registerEvents(this, Fruitninja.i());
-	}
+    public int score = 0;
 
-	public void deRegister() {
-		HandlerList.unregisterAll(this);
-	}
-	
-	public void stop() {
-		deRegister();
-		Bukkit.getScheduler().cancelTask(tickTask);
-	}
+    boolean clicking = false;
+    int clickingdelay = 0;
+
+    public Game(Player player, Location location) {
+        this.player = player;
+        gameLocation = location;
+        start();
+    }
+
+    public void addPoint() {
+        score++;
+        player.setLevel(score);
+    }
+
+    public int lives = 5;
+
+    public void removeLife() {
+        lives--;
+        if (lives == 0) {
+            stop();
+        }
+    }
+
+    public int maxDelay = 80;
+    public int delay = 20;
+
+    public void tick() {
+        if (clickingdelay++ > 5) clicking = false;
+        if (delay-- < 0) {
+            delay = new Random().nextInt(maxDelay);
+            if (maxDelay > 20) maxDelay--;
+            fruitlist.add(new Fruit(this));
+        }
+        Location location = lookingLocation();
+        if (rainbowTick++ == 255) {
+            rainbowTick = 0;
+        }
+        if (clicking) {
+            displayParticle(location, Color.getHSBColor(rainbowTick / 255F, 0.5F, 0.5F));
+        }
+        for (int i = 0; i < fruitlist.size(); i++)
+            if (fruitlist.get(i).tick(location)) {
+                fruitlist.remove(i);
+            }
+    }
+
+    public Location lookingLocation() {
+        double z = distance + gameLocation.getZ();
+        double playerdistance = player.getLocation().getZ() - z;
+        double y = playerdistance * Math.tan(Math.toRadians(player.getLocation().getPitch())) + 1.7 + player.getLocation().getY();
+        double x = playerdistance * Math.tan(Math.toRadians(player.getLocation().getYaw())) + player.getLocation().getX();
+        return new Location(player.getWorld(), x, y, z);
+    }
+
+    public void displayParticle(Location location, Color color) {
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(
+                new PacketPlayOutWorldParticles(EnumParticle.REDSTONE, false, (float) location.getX(),
+                        (float) location.getY(), (float) location.getZ(), (float) (color.getRed() + 1) / 255 - 1,
+                        (float) (color.getGreen() + 1) / 255, (float) (color.getBlue() + 1) / 255, 1F, 0));
+    }
+
+
+    @EventHandler
+    public void onClick(PlayerInteractEvent event) {
+        if (event.getPlayer() == player) {
+            clicking = true;
+            clickingdelay = 0;
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onClickArEntity(PlayerInteractAtEntityEvent event) {
+        if (event.getPlayer() == player) {
+            clicking = true;
+            clickingdelay = 0;
+            event.setCancelled(true);
+
+        }
+    }
+
+    @EventHandler
+    public void onClickEntity(PlayerInteractEntityEvent event) {
+        if (event.getPlayer() == player) {
+            clicking = true;
+            clickingdelay = 0;
+            event.setCancelled(true);
+
+        }
+    }
+
+
+    void start() {
+        tickTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(Fruitninja.i(), this::tick, 1, 1);
+        player.teleport(gameLocation);
+        player.setGameMode(GameMode.ADVENTURE);
+        player.setItemInHand(new ItemStack(Material.BONE));
+        register();
+    }
+
+    public List<Fruit> fruitlist = new ArrayList<>();
+
+    public void register() {
+        Bukkit.getPluginManager().registerEvents(this, Fruitninja.i());
+    }
+
+    public void deRegister() {
+        HandlerList.unregisterAll(this);
+    }
+
+    public void stop() {
+        deRegister();
+        Bukkit.getScheduler().cancelTask(tickTask);
+    }
 
 }
